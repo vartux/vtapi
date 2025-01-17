b76631cbcd2c6257afde55cc653eed23ceec87f0
victor tortolero casa <vart1030@gmail.com>
2025-01-17 16:04:10 -0400

    first commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/tokens/create', function () {
+    $token = Auth::user()->createToken("firsttoken");
+
+    return ['token' => $token->plainTextToken];
+});
