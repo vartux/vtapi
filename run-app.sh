1a375e11c910af28cf623266e3d07596d19f8d21
victor tortolero <vart1030@gmail.com>
2025-01-20 14:43:03 -0400

    feat(vite): confiugracion de css en vite

--- run-app.sh
+++ run-app.sh
@@ -2,15 +2,4 @@
 # Make sure this file has executable permissions, run `chmod +x run-app.sh`
 # Run migrations, process the Nginx configuration template and start Nginx
 
-npm run build
-
-# Clear cache
-php artisan optimize:clear
-
-# Cache the various components of the Laravel application
-php artisan config:cache
-php artisan event:cache
-php artisan route:cache
-php artisan view:cache
-
 php artisan migrate --force && node /assets/scripts/prestart.mjs /assets/nginx.template.conf  /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
